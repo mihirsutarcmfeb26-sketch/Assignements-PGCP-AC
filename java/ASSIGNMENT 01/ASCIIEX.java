fde040f668a6cf8cd3e2ae2f5677e3391d888ab2
mihirsutarcmfeb26-sketch <mihir.sutar.cmfeb26@gmail.com>
2026-03-30 23:29:33 +0530

    Q4

---
+++ java/ASSIGNMENT 01/ASCIIEX.java
@@ -0,0 +1,16 @@
+class ASCIIEX{
+   
+
+   public static void main(String[] args){
+
+          char c = 'A';
+
+          int x = (int) c;
+
+          System.out.println("c");
+          System.out.println("x");
+
+   }
+    
+    
+    }
