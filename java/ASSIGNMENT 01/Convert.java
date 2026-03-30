629322649549a81cf216dc253845be764db3848a
mihirsutarcmfeb26-sketch <mihir.sutar.cmfeb26@gmail.com>
2026-03-30 23:22:15 +0530

    Q3

---
+++ java/ASSIGNMENT 01/Convert.java
@@ -0,0 +1,18 @@
+class Convert{
+
+    public static void main(String[] args) {
+        int a = 10;
+
+        double d = 67.69;
+
+        double x = (int)a;
+        int y = (int) d;
+
+        System.err.println("a");
+        System.err.println("d");
+        System.err.println("x");
+        System.err.println("y");
+        
+
+    }
+}
