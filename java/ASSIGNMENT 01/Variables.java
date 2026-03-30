8a182c7485ae5be9de1649f32093059419d61792
mihirsutarcmfeb26-sketch <mihir.sutar.cmfeb26@gmail.com>
2026-03-30 22:55:55 +0530

    ADD

---
+++ java/ASSIGNMENT 01/Variables.java
@@ -0,0 +1,26 @@
+
+class Variables{
+
+    void show(){
+
+        int a = 10;
+
+        double d= 10.00;
+        float f = 10;
+        char c= 'd';
+        short s = 's';
+        long l = 'm';
+
+    }
+    public static void main(String[] args) {
+        Variables vr = new Variables();
+        vr.show();
+        System.err.println("a");
+        System.err.println("d");
+        System.err.println("f");
+        System.err.println("c");
+        System.err.println("s");
+        System.err.println("l");
+        
+    }
+}
