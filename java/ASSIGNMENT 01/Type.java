b88ed0ef9e690a11826299b28804e71e1722644d
mihirsutarcmfeb26-sketch <mihir.sutar.cmfeb26@gmail.com>
2026-03-30 23:15:42 +0530

    ASSQ2

---
+++ java/ASSIGNMENT 01/Type.java
@@ -0,0 +1,21 @@
+class Type {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 3.5;
+
+        // Addition
+        double add = a + b;
+
+        // Division
+        double div = a / b;
+
+        // Type casting to int
+        int result = (int) div;
+
+        System.out.println("Addition: " + add);
+        System.out.println("Division: " + div);
+        System.out.println("After Type Casting (int): " + result);
+    }
+}
